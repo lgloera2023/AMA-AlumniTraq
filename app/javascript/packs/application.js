ce46ea7c60818064f2797f3537373035096b64c3
Momo16136 <momo16136@tamu.edu>
2022-11-02 14:06:43 -0500

    bootstrap install

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,6 +7,8 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
+import "bootstrap/dist/js/boostrap"
+import "bootstrap/dist/css/boostrap"
 require("jquery")
 require("jquery-ui")
 import $ from 'jquery'
